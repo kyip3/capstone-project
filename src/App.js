fbaa6e03c91dee0001dcd481d44540e487198aee
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-22 15:49:58 +0800

    testing nested routing /home/shop..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
 
+const Shop = () => {
+  return <div>I am shop page</div>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home/>}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
