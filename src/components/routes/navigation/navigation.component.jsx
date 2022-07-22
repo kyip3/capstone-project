427192564885e0ae22c1f5df8a464b6b7e726ad8
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-22 16:05:06 +0800

    added logo link and shop link navigation

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,11 +1,18 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div>
-        <h1>i am the navigation bar</h1>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="nav-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
