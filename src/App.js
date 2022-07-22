33cd8566df5b2b680e90be51dce5eeac141fb518
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-22 16:00:59 +0800

    added/moved navigation component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>i am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 const Shop = () => {
   return <div>I am shop page</div>;
 };
